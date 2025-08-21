361090d14840f497e18449fca002615436c4a486
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-08-21 17:57:44 -0600

    feat(db): Write database logic for use SQLModel as schema and table database managment

---
+++ app/database.py
@@ -0,0 +1,27 @@
+import dotenv
+import os
+from typing import Annotated
+from sqlmodel import Session, SQLModel, create_engine
+from fastapi import Depends
+from .models import Post
+
+dotenv.load_dotenv()
+
+
+# Postgres Engine
+
+postgresql_url = os.environ.get("POSTGRESQL_URL")
+engine = create_engine(postgresql_url)
+
+# Create the database and tables
+def create_db_and_tables():
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+
+# Dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDependency = Annotated[Session, Depends(get_session)]
---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+from sqlalchemy import text
+
+
+class Post(SQLModel, table=True):
+    __tablename__ = "posts"
+    id: int = Field(primary_key=True, nullable=False)
+    title: str = Field(max_length=100, nullable=False)
+    content: str = Field(nullable=False)
+    published: bool | None = Field(
+        nullable=False,
+        default=True,
+        sa_column_kwargs={"server_default": "true"}
+    )
+    created_at: datetime | None = Field(
+        nullable=False,
+        default=datetime.now(timezone.utc), 
+        sa_column_kwargs={"server_default": text("now()")}
+    )
